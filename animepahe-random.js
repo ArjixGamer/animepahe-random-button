b3303721afc33af1d1707e68866e0434e8030a6d
Arjix <53124886+ArjixGamer@users.noreply.github.com>
2021-03-22 02:55:20 +0200

    Update animepahe-random.js

--- animepahe-random.js
+++ animepahe-random.js
@@ -1,12 +1,11 @@
 // ==UserScript==
 // @name         AnimePahe Random Anime
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Adds a random anime button to animepahe
 // @author       Arjix
 // @match        *://*animepahe.com/*
 // @require      https://code.jquery.com/jquery-3.5.1.min.js
-// @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
 function getRandomInt(min, max) {
@@ -19,14 +18,10 @@ function getRandomInt(min, max) {
 (function() {
     'use strict';
     const getRandomAnime = () => {
-        GM_xmlhttpRequest({
-            method: "GET",
-            url: "https://animepahe.com/anime",
-            onload: function(res) {
-                const ALL_ANIME = Array.from(res.response.matchAll(/\<div class=\"col-12 col-md-6\"\>\n\<a href=\"(.*?)\"/g)).map(anime => anime[1])
-                const index = getRandomInt(1, ALL_ANIME.length - 1)
-                document.location.href = "https://animepahe.com" + ALL_ANIME[index]
-            }
+        fetch("https://animepahe.com/anime").then(res => res.text()).then(function(res) {
+            const ALL_ANIME = Array.from(res.matchAll(/\<div class=\"col-12 col-md-6\"\>\n\<a href=\"(.*?)\"/g)).map(anime => anime[1])
+            const index = getRandomInt(1, ALL_ANIME.length - 1)
+            document.location.href = "https://animepahe.com" + ALL_ANIME[index]
         })
     }
     const button = `<li class="nav-item">
