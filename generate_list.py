ef0920d73d5dab8a227bb0dcbf523ca74397db7d
Arjix <53124886+ArjixGamer@users.noreply.github.com>
2021-05-16 19:39:34 +0300

    Update generate_list.py

--- generate_list.py
+++ generate_list.py
@@ -2,7 +2,7 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-r = requests.get("https://animepahe.com/anime").text
+r = requests.get("https://animepahe.com/anime", headers={"referer": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"}).text
 soup = BeautifulSoup(r, 'html.parser')
 
 ALL_ANIME = [x['href'] for x in soup.select(".col-12.col-md-6 > a[href]")]
